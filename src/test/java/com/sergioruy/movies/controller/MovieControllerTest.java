1bec60874e7b82fd9b187db1a9acc2b27ce4b38c
SergioRuyDev <sergioruyenator@gmail.com>
2021-12-17 11:05:40 +0400

    Mock MVC bean added.

--- src/test/java/com/sergioruy/movies/controller/MovieControllerTest.java
+++ src/test/java/com/sergioruy/movies/controller/MovieControllerTest.java
@@ -1,10 +1,15 @@
 package com.sergioruy.movies.controller;
 
-import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import com.sergioruy.movies.service.MovieService;
+import io.restassured.http.ContentType;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
 
 @WebMvcTest
 public class MovieControllerTest {
@@ -12,15 +17,24 @@ public class MovieControllerTest {
     @Autowired
     private MovieController movieController;
 
+    @MockBean
+    private MovieService movieService;
+
     // This method is for show to Spring only my movie controller will be test. Just in case if I have many controllers
     // I have to show which controller I will test.
     @BeforeEach
     public void setup() {
-        RestAssuredMockMvc.standaloneSetup(this.movieController);
+        standaloneSetup(this.movieController);
     }
 
     @Test
     public void shouldReturnSuccess_WhenGetMovie() {
 
+        given()
+                .accept(ContentType.JSON)
+        .when()
+                .get("/movies/{code}", 1L)
+        .then()
+                .statusCode(HttpStatus.SC_OK);
     }
 }
